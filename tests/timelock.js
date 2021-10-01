3cb5e28300ad66d609202b40bd9a8b8230a74539
imprfekt <5047969+imprfekt@users.noreply.github.com>
2021-10-01 23:55:29 +0200

    WIP vesting tests

--- tests/timelock.js
+++ tests/timelock.js
@@ -14,7 +14,9 @@ describe('timelock', () => {
     const depositor = provider.wallet;
     const program = anchor.workspace.Timelock;
     const pda = Keypair.generate();
+    const pdaCliff = Keypair.generate();
     const pdaTokenAcc = Keypair.generate();
+    const pdaCliffTokenAcc = Keypair.generate();
     const beneficiary = provider.wallet; //todo update to something else, e.g. known address or Keypair.generate();
 
     let mint;
@@ -22,11 +24,12 @@ describe('timelock', () => {
     let beneficiaryTokenAcc;
 
     let pdaSigner; //needed to sign transactions in the name of PDA account during withdrawal/cancel.
+    let pdaCliffSigner;
 
     const start = new BN(+new Date() / 1000 + 5); //divide by 1000 since unix timestamp is in seconds and add 5 seconds
     const end = new BN((+new Date()) / 1000 + 60); //one min later
-    const deposited_amount = new BN(1337);
-    let withdrawAmount;
+    const period = period || new BN(1);//defaults to 1 second
+    const depositedAmount = new BN(1337);
 
     it("Initialize test state", async () => {
         const [_mint, _mint_authority] = await common.createMintAndVault(
@@ -43,7 +46,7 @@ describe('timelock', () => {
         // should we use anchor.utils.token.associatedAddress()?
     })
 
-    it("Vesting Contract Creation", async () => {
+    it("Create Vesting Contract w/out the cliff", async () => {
         //if seeds are known, signer can be derived.
         let [_pdaSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
             [pda.publicKey.toBuffer()],//(Seeds can be anything but we decided those will be serialized pda's pubkey
@@ -54,13 +57,68 @@ describe('timelock', () => {
         console.log('pdasig', pdaSigner.toBase58(), 'pda', pda.publicKey.toBase58(), 'buffer', pda.publicKey.toBuffer())
         console.log('nonce', nonce);
 
+        
         const tx = await program.rpc.create(
             //order of the parameters must match the ones in program
             beneficiary.publicKey, //beneficiary
-            deposited_amount,
+            depositedAmount,
             start,
             end,
+            period,
             nonce,
+            null,
+            null,
+            {
+                accounts: {
+                    pda: pda.publicKey,
+                    pdaSigner,
+                    pdaTokenAcc: pdaTokenAcc.publicKey,
+                    depositorTokenAcc,
+                    depositor: depositor.publicKey,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    systemProgram: SystemProgram.programId
+                },
+                signers: [pda, pdaTokenAcc],
+                instructions: [
+                    // await program.account.vestingContract.createInstruction(pda),
+                    ...(await common.createTokenAccountInstrs(provider, pdaTokenAcc.publicKey, mint, pdaSigner))
+                ]
+            })
+
+        const vault = await program.provider.connection.getAccountInfo(pdaTokenAcc.publicKey)
+        const depositor_token_acc = await program.provider.connection.getAccountInfo(depositorTokenAcc)
+
+        const acc = await program.account.vestingContract.fetch(pda.publicKey)
+        const pda_ata = common.token.parseTokenAccountData(vault.data);
+        const dep_ata = common.token.parseTokenAccountData(depositor_token_acc.data)
+        // console.log('contract', acc,
+        //     'pda ata', pda_ata,
+        //     'dep ata', dep_ata);
+        console.log('deposited during contract creation: ', depositedAmount.toNumber(), pda_ata.amount)
+        assert.ok(depositedAmount.toNumber() === pda_ata.amount);
+    });
+
+    it("Create Vesting Contract with the cliff", async () => {
+        const cliff = start.add(new BN(20));
+        const cliffAmount = 
+        //if seeds are known, signer can be derived.
+        let [_pdaSigner, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+            [pdaCliff.publicKey.toBuffer()],//(Seeds can be anything but we decided those will be serialized pda's pubkey
+            program.programId
+        );
+
+        pdaCliffSigner = _pdaSigner;
+
+        const tx = await program.rpc.create(
+            //order of the parameters must match the ones in program
+            beneficiary.publicKey,
+            depositedAmount,
+            start,
+            end,
+            period,
+            nonce,
+            cliff,
+            cliff_amount,
             {
                 accounts: {
                     pda: pda.publicKey,
@@ -87,10 +145,9 @@ describe('timelock', () => {
         // console.log('contract', acc,
         //     'pda ata', pda_ata,
         //     'dep ata', dep_ata);
-        console.log('deposited during contract creation: ', deposited_amount.toNumber(), pda_ata.amount)
-        assert.ok(deposited_amount.toNumber() === pda_ata.amount);
+        console.log('deposited during contract creation: ', depositedAmount.toNumber(), pda_ata.amount)
+        assert.ok(depositedAmount.toNumber() === pda_ata.amount);
     });
-
     // it("Withdraws from a contract", async () => {
     //     setTimeout(async () => {
     //         // beneficiaryTokenAcc = await utils.token.associatedAddress({mint, owner: beneficiary.publicKey})
@@ -123,7 +180,7 @@ describe('timelock', () => {
     //         const newBeneficiaryAta = await program.provider.connection.getAccountInfo(beneficiaryTokenAcc);
     //         const newBeneficiaryAmount = common.token.parseTokenAccountData(newBeneficiaryAta.data).amount;
     //         const pdaData = (await program.account.vestingContract.fetch(pda.publicKey));
-    //         console.log('deposited_amount', pdaData.depositedAmount, 'withdrawn', pdaData.withdrawn, 'amount', withdrawAmount)
+    //         console.log('depositedAmount', pdaData.depositedAmount, 'withdrawn', pdaData.withdrawn, 'amount', withdrawAmount)
     //         assert.ok(withdrawAmount.eq(new BN(oldPdaAmount - newPdaAmount)))
     //         assert.ok(withdrawAmount.eq(new BN(newBeneficiaryAmount - oldBeneficiaryAmount)))
     //         assert.ok(pdaData.withdrawn.eq(withdrawAmount))
@@ -156,7 +213,7 @@ describe('timelock', () => {
     //         const newBeneficiaryAta = await program.provider.connection.getAccountInfo(beneficiaryTokenAcc);
     //         const newBeneficiaryAmount = common.token.parseTokenAccountData(newBeneficiaryAta.data).amount;
     //         const pdaData = (await program.account.vestingContract.fetch(pda.publicKey));
-    //         console.log('deposited_amount', pdaData.depositedAmount.toNumber(), 'withdrawn', pdaData.withdrawn.toNumber(), 'beneficiary amount', newBeneficiaryAmount.toNumber())
+    //         console.log('depositedAmount', pdaData.depositedAmount.toNumber(), 'withdrawn', pdaData.withdrawn.toNumber(), 'beneficiary amount', newBeneficiaryAmount.toNumber())
     //         assert.ok(newPdaAmount.toNumber() === 0)
     //
     //     }, 9000);
